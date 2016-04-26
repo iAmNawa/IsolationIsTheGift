9ec78d5f33a0c8a2147c31bc8694ac505ce1d877
Paul Borawski <paulborawski14@gmail.com>
2016-04-26 11:52:53 -0700

    finished first page

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
 (function(){
   var tl1 = new TimelineMax();
   tl1.from('#h_one', 4, { x: 1000 })
-  .from('#h_three', 4, { x: -1000 }, 'h_one-=2')
+  .from('#h_three', 4, { x: -1000 }, 'h_one-=3.5')
   .from('#h_two', 1.9, { x: 1000 }, 'h_one+=1');
 }());
