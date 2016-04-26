5ab5b56367a173968d50316352ca5e4bcbc62b44
Paul Borawski <paulborawski14@gmail.com>
2016-04-26 13:21:38 -0700

    fixed animation

--- public/script.js
+++ public/script.js
@@ -2,5 +2,5 @@
   var tl1 = new TimelineMax();
   tl1.from('#h_one', 4, { x: 1000 })
   .from('#h_three', 4, { x: -1000 }, 'h_one-=3.5')
-  .from('#h_two', 1.9, { x: 1000 }, 'h_one+=1');
+  .from('#h_two', 1.9, { x: 1000, y: -400, rotation: 1000 }, 'h_one+=1');
 }());
