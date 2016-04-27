89c6df830ab1966dcf96d516dcd5685a7e884111
Paul Borawski <paulborawski14@gmail.com>
2016-04-26 19:17:53 -0700

    secondpage/video

---
+++ public/main/script2.js
@@ -0,0 +1,24 @@
+(function(){
+    var tl1 = new TimelineMax();
+    tl1.from('#myAd_ltr01', 2, { x: 1500 })
+   .from('#myAd_ltr02', 2, {x:1500}, 'myAd_ltr01-=1.8')
+   .from('#myAd_ltr03', 2, {x:1500}, 'myAd_ltr02-=1.8')
+   .from('#myAd_ltr04', 2, {x:1500}, 'myAd_ltr03-=1.8')
+   .from('#myAd_ltr05', 2, {x:1500}, 'myAd_ltr04-=1.8')
+   .from('#myAd_ltr06', 2, {x:1500}, 'myAd_ltr05-=1.8')
+   .from('#myAd_ltr07', 2, {x:1500}, 'myAd_ltr06-=1.8')
+   .from('#myAd_ltr08', 2, {x:1500}, 'myAd_ltr07-=1.8')
+   .from('#myAd_ltr09', 2, {x:1500}, 'myAd_ltr08-=1.8')
+   .from('#myAd_ltr10', 2, {x:1500}, 'myAd_ltr09-=1.8')
+   .from('#myAd_ltr11', 2, {x:1500}, 'myAd_ltr10-=1.8')
+   .from('#myAd_ltr12', 2, {x:1500}, 'myAd_ltr11-=1.8')
+   .from('#myAd_ltr13', 2, {x:1500}, 'myAd_ltr12-=1.8')
+   .from('#myAd_ltr14', 2, {x:1500}, 'myAd_ltr13-=1.8')
+   .from('#myAd_ltr15', 2, {x:1500}, 'myAd_ltr14-=1.8')
+   .from('#myAd_ltr16', 2, {x:1500}, 'myAd_ltr15-=1.8')
+   .from('#myAd_ltr17', 2, {x:1500}, 'myAd_ltr16-=1.8')
+   .from('#myAd_ltr18', 2, {x:1500}, 'myAd_ltr17-=1.8')
+   .from('#myAd_ltr19', 2, {x:1500}, 'myAd_ltr18-=1.8')
+   .from('#myAd_ltr20', 2, {x:1500}, 'myAd_ltr19-=1.8')
+   .from('#myAd_ltr21', 2, {x:1500}, 'myAd_ltr20-=1.8');
+}());
